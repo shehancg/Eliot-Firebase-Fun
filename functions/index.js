fc962f66bda7653e09878624735d09f811dac5a3
Shehan Gunawardena <64646307+shehancg@users.noreply.github.com>
2023-11-30 21:19:55 +0530

    Added scheduler updateValuesProductionFlow to reset values of productionFlowAcrossOperations collection

--- functions/index.js
+++ functions/index.js
@@ -564,8 +564,9 @@ exports.updateGraphDefectCounts = functions.database.ref("/defects/{obbID}/RMGde
       }
     });
 
-// SCHEDULER FUNCTION FOR TURNING VALUES IN GRAPHS
+// SCHEDULER FUNCTIONS TO RESET VALUES IN GRAPHS
 
+// COLLECTION productionEfficiencyAcrossOperations
 // Define the scheduled function
 // eslint-disable-next-line max-len
 exports.updateValuesProductionEff = functions.pubsub.schedule("00 21 * * *") // Runs every day at 11:00 PM
@@ -615,4 +616,52 @@ exports.updateValuesProductionEff = functions.pubsub.schedule("00 21 * * *") // 
       }
     });
 
+// COLLECTION productionFlowAcrossOperations
+// eslint-disable-next-line max-len
+exports.updateValuesProductionFlow = functions.pubsub.schedule("18 21 * * *") // Runs every day at 11:00 PM
+    .timeZone("Asia/Colombo") // Set the timezone to Colombo
+    .onRun(async (context) => {
+      try {
+        const db = admin.database();
+
+        // Get a snapshot of all OBBS nodes
+        const obbsSnapshot = await db.ref("/graphs").once("value");
+
+        // Convert the snapshot to a JavaScript object
+        const obbsData = obbsSnapshot.toJSON();
+
+        // Check if the data is available and is iterable
+        if (obbsData) {
+          // Iterate through each OBBS node
+          for (const obbsId of Object.keys(obbsData)) {
+            // Loop through collections from 0 to 11 for each OBBS node
+            for (let i = 0; i < 12; i++) {
+              // eslint-disable-next-line max-len
+              const collectionRef = db.ref(`/graphs/${obbsId}/productionFlowAcrossOperations/${i}`);
+
+              // eslint-disable-next-line max-len
+              // Loop through collections from 0 to 3 inside each outer collection
+              for (let j = 0; j < 4; j++) {
+                const innerCollectionRef = collectionRef.child(`${j}`);
+
+                // Update the values x and y to 'operation' and -1 respectively
+                await innerCollectionRef.update({
+                  x: "operation",
+                  y: -1,
+                });
+
+                // eslint-disable-next-line max-len
+                console.log(`Values updated at ${innerCollectionRef.toString()}`);
+              }
+            }
+          }
+        }
+
+        console.log("Scheduled function executed successfully.");
+        return null;
+      } catch (error) {
+        console.error("Error in scheduled function:", error);
+        return null;
+      }
+    });
 
